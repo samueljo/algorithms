b33dfd972c2d8ec7898fc7c14f04cc2d49feb963
Sam Jo <samjo891@gmail.com>
2016-11-11 11:13:13 -0500

    Initialize npm

---
+++ algorithms.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react';
+// import Queens ...
+// import MazeSolver ...
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    document.getElementById("main")
+  );
+});
